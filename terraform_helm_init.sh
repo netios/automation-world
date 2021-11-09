c5fe69b7098115d64a165a984088f94b27bb73dd
SEAN.LEE <icarusean@gmail.com>
2021-11-09 15:59:39 +0900

    Update terraform_helm_init.sh
    업데이트 관리 방식으로 변경

--- terraform_helm_init.sh
+++ terraform_helm_init.sh
@@ -9,6 +9,9 @@ terraform version
 
 # helm
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 helm version
