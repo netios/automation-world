23288d14cff6c06fa50f561437ba69c76b609241
SEAN.LEE <icarusean@gmail.com>
2021-12-04 03:05:28 +0900

    Update ps_zsh.sh

--- ps_zsh.sh
+++ ps_zsh.sh
@@ -2,4 +2,33 @@ sudo apt install zsh -y
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh -y
 sed -i "s/\=\"robbyrussell/\=\"jonathan/g" ~/.zshrc
+sed -i "\$aalias h=\"history -i\"" ~/.zshrc
+sed -i "\$aalias hs=\"history -i | grep\"" ~/.zshrc
+cat <<EOF >> ~/.zshrc                                                                                                                                               ──(Fri,Dec03)─┘
+#_____ history for zsh
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=10000000
+SAVEHIST=10000000
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing nonexistent history.
+EOF
+echo "#_____ [zsh_plugin] Auto Suggestions" >> ~/.zshrc 
+sudo apt install zsh-autosuggestions -y
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "#_____ [zsh_plugin] Syntax Highlighting" >> ~/.zshrc 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 source ~/.zshrc
+#_____ other apps
+sudo apt install tree -y
