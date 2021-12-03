95b181c2276bc7888d4a1728656a7ae9f222cfc9
SEAN.LEE <icarusean@gmail.com>
2021-12-04 01:54:26 +0900

    Update ps_zsh.sh

--- ps_zsh.sh
+++ ps_zsh.sh
@@ -1,3 +1,4 @@
 sudo apt install zsh -y
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+sh install.sh -y
+sed -i "s/\=\"robbyrussell/\=\"jonathan/g" ~/.zshrc
