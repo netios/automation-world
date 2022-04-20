4be1a83ab2d51638b89846b7c2a5f5e1a000ed60
SEAN.LEE <icarusean@gmail.com>
2022-04-21 00:55:26 +0900

    Create terraform_init.sh

---
+++ k8s/terraform_init.sh
@@ -0,0 +1,21 @@
+# ubuntu 
+# terraform
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+terraform version
+
+# helm
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+helm version
+
+# other
+sudo apt-get install tree
+sudo apt-get autoremove
