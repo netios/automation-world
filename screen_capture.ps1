9dd835f29aa1a1ee36db2ee2da0f8092ce61be64
SEAN.LEE <icarusean@gmail.com>
2022-04-21 01:23:21 +0900

    Delete screen_capture.ps1

--- screen_capture.ps1
+++
@@ -1,8 +0,0 @@
-function Set-ClipboardFromScreenshot ([Drawing.Rectangle]$Bounds) {
-  $bmp = New-Object Drawing.Bitmap $Bounds.width, $Bounds.height
-  $graphics = [Drawing.Graphics]::FromImage($bmp)
-  $graphics.CopyFromScreen($Bounds.Location, [Drawing.Point]::Empty, $Bounds.size)
-  [Windows.Forms.Clipboard]::SetImage($bmp)
-}
-
-Set-ClipboardFromScreenshot ( [Drawing.Rectangle]::FromLTRB(-1918,66,-895,735) )
