1e2a42210b1b91c9e6dedb5650a242ab6964f89d
SEAN.LEE <icarusean@gmail.com>
2022-07-27 03:58:32 +0900

    Create kubecolor_install.sh

---
+++ k8s/kubecolor_install.sh
@@ -0,0 +1,21 @@
+#===========================
+RUN_DIRECTORY=~/bin/
+DIRECTORY=~/bin/kubecolor/
+APPFILE=kubecolor
+LINKFILE=k
+#===========================
+if [ ! -d "$DIRECTORY" ]; then
+  # echo $DIRECTORY not exist.
+  mkdir -p $DIRECTORY
+fi
+cd $DIRECTORY
+curl -L -s $(curl -L  -s https://github.com/hidetatz/kubecolor/releases/latest | egrep '<a.+Linux_x86_64.tar.gz' | awk -F\" '{print "https://github.com"$2}') | tar -zx
+cd $RUN_DIRECTORY
+if [ -f "$LINKFILE" ]; then
+  # echo $LINKFILE exist.
+  rm $LINKFILE
+  # ln -s -f ${DIRECTORY}${APPFILE} $LINKFILE
+fi
+ln -s ${DIRECTORY}${APPFILE} $LINKFILE
+chmod +x $LINKFILE
+echo "==== kubecolor install done. ===="
